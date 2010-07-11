67d6ba2738f955b3faab6ddea678422ecaf20506
wal <walter.mcconnell@gmail.com>
2010-07-11 23:35:49 +0100

    adding new relic

--- my_app.rb
+++ my_app.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 
+configure :production do
+    ENV['APP_ROOT'] ||= File.dirname(__FILE__)
+    $:.unshift "#{ENV['APP_ROOT']}/vendor/plugins/rpm/lib"
+    require 'newrelic_rpm'
+end
+
+
 get '/' do
   "Hello"
 end
