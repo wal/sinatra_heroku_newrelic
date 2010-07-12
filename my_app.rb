5cfde3d24157adb8d6a4d8af58c8fe17184d59cd
wal <walter.mcconnell@gmail.com>
2010-07-12 13:06:21 +0100

    remove plugin

--- my_app.rb
+++ my_app.rb
@@ -2,9 +2,7 @@ require 'rubygems'
 require 'sinatra'
 
 configure :production do
-    ENV['APP_ROOT'] ||= File.dirname(__FILE__)
-    $:.unshift "#{ENV['APP_ROOT']}/vendor/plugins/rpm/lib"
-    require 'newrelic_rpm'
+  require 'newrelic_rpm'
 end
 
 
